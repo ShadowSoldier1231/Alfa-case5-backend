a52740ffe7901e1d8ba4498b34e9e81b8a8a482c
mainPc <blacklight4422@bk.ru>
2026-08-19 18:12:14 +0300

    minor fixes

--- main/src/main/java/com/project/main/MainApplication.java
+++ main/src/main/java/com/project/main/MainApplication.java
@@ -10,7 +10,7 @@ import org.telegram.telegrambots.longpolling.starter.TelegramBotStarterConfigura
 
 
 @SpringBootApplication(exclude = {
-        TelegramBotStarterConfiguration.class,
+        TelegramBotStarterConfiguration.class
 })
 @ComponentScan
 @EnableScheduling
