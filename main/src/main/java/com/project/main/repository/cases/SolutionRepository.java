a557248a337fa5cb63b4db6eab3febe4fbc80a15
mainPc <blacklight4422@bk.ru>
2026-08-28 14:05:40 +0300

    updated SolutionRepository

--- main/src/main/java/com/project/main/repository/cases/SolutionRepository.java
+++ main/src/main/java/com/project/main/repository/cases/SolutionRepository.java
@@ -143,12 +143,12 @@ public interface SolutionRepository extends JpaRepository<Solution, Long> {
             value = """
         SELECT *
         FROM solution
-        WHERE id = :caseId
+        WHERE solution_id = :caseId
         """,
             countQuery = """
         SELECT COUNT(*)
         FROM solution
-        WHERE id = :caseId
+        WHERE solution_id = :caseId
         """,
             nativeQuery = true
     )
